70cca9fb6ab859035c70c30088d5d239942c6e4d
Ankur Agrawal <ankuragrawal9455@gmail.com>
2022-04-26 13:46:32 +0530

    Fixing bugs 2.0

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from .views import index, new_data, devices, add_device
 
 urlpatterns = [
     path("", index, name="index"),
-    path("data/<int:id>", new_data, name="data"),
+    path("data/<str:id>", new_data, name="data"),
     path("devices", devices, name="devices"),
-    path("add_device/<int:id>", add_device, name="add_devices"),
+    path("add_device/<str:id>", add_device, name="add_devices"),
 ]
