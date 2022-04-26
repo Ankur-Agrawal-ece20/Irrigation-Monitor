a7c826389b412f89bcc9384100baa7f9abc55103
Ankur Agrawal <ankuragrawal9455@gmail.com>
2022-04-26 16:08:51 +0530

    Adding cors

--- IrrigationSystem/settings.py
+++ IrrigationSystem/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+d(q1$za1xs=a72*_u+3@9htbu!4#cdp4(4q69thyl@k#6xk@%
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'IrrigationSystem.urls'
